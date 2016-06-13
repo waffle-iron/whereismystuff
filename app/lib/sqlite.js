2f857258ca103a3d06ce568759d788e044e38a49
Jonas A. Hulten <jonas@sldr.se>
2016-06-13 16:45:39 +0200

    Updated DB schema to include the inventory view
    Also added a generic statement preparation function. The idea is
    that other models will get this database object handed to them
    and will talk to it via prepared statements so as to avoid
    injection.

--- app/lib/sqlite.js
+++ app/lib/sqlite.js
@@ -19,19 +19,50 @@ this.createDatabase = function(filename, callback){
 function initDB(db){
   db.serialize(function(){
     // Create the base tables
-    db.run("CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT, name\
-    TEXT);");
-    db.run("CREATE TABLE item (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,\
-    category NUMERIC);");
-    db.run("CREATE TABLE location (id INTEGER PRIMARY KEY AUTOINCREMENT, name\
-    TEXT, inside INTEGER);");
-    db.run("CREATE TABLE entity (item INTEGER NOT NULL, location INTEGER NOT\
-    NULL, quantity INTEGER);");
+    db.run("CREATE TABLE category (\
+      id INTEGER PRIMARY KEY AUTOINCREMENT,\
+      name TEXT\
+    );");
+    db.run("CREATE TABLE item (\
+      id INTEGER PRIMARY KEY AUTOINCREMENT,\
+      name TEXT,\
+      category_id INTEGER\
+    );");
+    db.run("CREATE TABLE location (\
+      id INTEGER PRIMARY KEY AUTOINCREMENT,\
+      name TEXT,\
+      inside_id INTEGER\
+    );");
+    db.run("CREATE TABLE entity (\
+      id INTEGER PRIMARY KEY AUTOINCREMENT,\
+      item_id INTEGER NOT NULL,\
+      location_id INTEGER NOT NULL,\
+      quantity INTEGER\
+    );");
 
     // Add base data
-    db.run("INSERT INTO location (name) VALUES ('World')");
-    db.run("INSERT INTO category (name) VALUES ('Uncategorized')");
+    db.run("INSERT INTO location (name) VALUES ('World');");
+    db.run("INSERT INTO category (name) VALUES ('Uncategorized');");
+
+    // Add main view
+    db.run("CREATE VIEW inventory AS\
+      SELECT entity.id AS id, entity.quantity AS quantity, item.name AS name,\
+        category.name AS category, location.name AS location\
+      FROM entity\
+      INNER JOIN location\
+      ON entity.location_id = location.id\
+      INNER JOIN item\
+      ON entity.item_id = item.id\
+      INNER JOIN category\
+      ON item.category_id = category.id\
+      WHERE entity.quantity > 0\
+    ;");
   });
+}
+
+// Prepares a statement - the primary way the database will be accessed
+this.makeStatement = function(sql){
+  return this.db.prepare(sql);
 }
 
 // Close the database
