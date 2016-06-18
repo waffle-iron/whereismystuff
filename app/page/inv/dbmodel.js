17a1a76e15367f4f42de2b941393788d99bdd59e
Jonas A. Hulten <jonas@sldr.se>
2016-06-18 18:00:38 +0200

    Added population of category and location lists
    Tree-like view for the locaiton list is sort of unfinished, but it's the best
      idea I have at the moment.
    
    The default value "Category" or "Location" is disabled in some areas where I
      don't believe it would do any harm.

--- app/page/inv/dbmodel.js
+++ app/page/inv/dbmodel.js
@@ -1,3 +1,10 @@
+const treelib = require('tree');
+
+// Update function
+this.update = function(updateCallback){
+  this.buildLocationTree(updateCallback);
+}
+
 // Function to pass in the dbm, in lieu of a proper constructor
 this.passDBM = function(dbm){
   this.dbm = dbm;
@@ -5,8 +12,8 @@ this.passDBM = function(dbm){
 
 // Retrieve the top of the inventory
 this.getInvHead = function(callback){
-  stmt = this.dbm.makeStatement("SELECT * FROM inventory ORDER BY id DESC LIMIT\
-    30;");
+  var stmt = this.dbm.makeStatement("SELECT * FROM inventory ORDER BY id DESC\
+    LIMIT 30;");
   stmt.each(function(err, row){
     if(err == null){
       callback(row);
@@ -24,4 +31,69 @@ this.getInvHead = function(callback){
       throw err;
     }
   });
+}
+
+// Build the location tree
+this.buildLocationTree = function(complete){
+  this.locationTree = new treelib.Root();
+  var ltree = this.locationTree;
+  var stmt = this.dbm.makeStatement("SELECT * FROM location ORDER BY id ASC;");
+  stmt.all(function(err, rows){
+    // Handle the root
+    var rootnode = new treelib.Node(null, {
+      'id' : rows[0].id,
+      'name' : rows[0].name
+    });
+    ltree.branch.append(rootnode);
+    // Handle the rest of the tree
+    for(i = 1; i < rows.length; i++){
+      ltree.insert(new treelib.Node(null, {
+        'id' : rows[i].id,
+        'name' : rows[i].name
+      }), rows[i].inside_id, rows[i].id);
+    }
+    stmt.finalize();
+    if(complete != undefined){
+      complete();
+    }
+  });
+}
+
+// List the categories
+this.getCategories = function(callback){
+  var stmt = this.dbm.makeStatement("SELECT * FROM category ORDER BY name\
+  ASC;");
+  stmt.each(function(err, row){
+    if(err == null){
+      callback(row);
+    }
+    else{
+      throw err;
+    }
+  },
+  function(err, num){
+    if(err == null){
+      console.log("Got " + num + " rows");
+      stmt.finalize();
+    }
+    else{
+      throw err;
+    }
+  });
+}
+
+// Recurses over the location tree, appends a prefix to the name. Calls the
+// callback for each item
+this.printTreeRecurse = function(prefix, branch, callback){
+  var i = 0;
+  for(i = 0; i < branch.nodes.length; i++){
+    callback({id: branch.nodes[i].id,
+              name: prefix + branch.nodes[i].data.name});
+    this.printTreeRecurse('>' + prefix, branch.nodes[i].children, callback);
+  }
+}
+
+// List the locations
+this.getLocations = function(callback){
+  this.printTreeRecurse('', this.locationTree.branch, callback);
 }
--- app/page/inv/index.js
+++ app/page/inv/index.js
@@ -18,9 +18,37 @@ function insertRowIntoTable(table){
     table.append("<tr><td><input type=\"checkbox\" id=\"entity-checkbox-" +
                  row.id + "\" value=\"" + row.id + "\"></td><td>" + row.quantity +
                  "</td><td>" + row.name + "</td><td>" + row.category +
-                 "</td><td>" + row.location + "</td></tr>")
+                 "</td><td>" + row.location + "</td></tr>");
   }
   return insertFunction;
+}
+
+// Insert option into select-list function
+function insertOptionIntoSelect(select){
+  var insertFunction = function(row){
+    select.append("<option data-id=\"" + row.id + "\">" + row.name +
+                  "</option>");
+  }
+  return insertFunction;
+}
+
+// Update the interface selector lists
+function updateLists(){
+  // Push category lists
+  var catListList = ['#filter-category', '#add-stuff-category',
+                     '#manage-delete-category'];
+  for(i = 0; i < catListList.length; i++){
+    var select = $(catListList[i]);
+    dbModel.getCategories(insertOptionIntoSelect(select));
+  }
+  // Push location lists
+  var locListList = ['#filter-location', '#add-stuff-location',
+                     '#manage-move-location-into', '#manage-move-location-from',
+                     '#manage-add-location', '#manage-delete-location'];
+  for(i = 0; i < locListList.length; i++){
+    var select = $(locListList[i]);
+    dbModel.getLocations(insertOptionIntoSelect(select));
+  }
 }
 
 // Site IO
@@ -31,4 +59,7 @@ $(document).ready(function(){
   var table = $('#inventory')
   // Get the inventory data and plug it into the table
   dbModel.getInvHead(insertRowIntoTable(table));
+  // While we're at it, populate the interface selector lists
+  dbModel.update(updateLists);
+
 });
