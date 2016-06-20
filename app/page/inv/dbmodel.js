8cb8450961cae03e0a91bf15f7bdd03df99e35e8
Jonas A. Hulten <jonas@sldr.se>
2016-06-20 11:23:57 +0200

    Implemented Manage tab
    Support for adding, removing, and moving categories and locations in
      place. Count-guard protects non-empty (or parent, in the case of locations)
      categories or locations from being removed.
    
    Also bugfixed an implementation issue in the location tree. It can now handle
      the database being out of order at the cost of some runtime. Tolerable, I say

--- app/page/inv/dbmodel.js
+++ app/page/inv/dbmodel.js
@@ -46,12 +46,20 @@ this.buildLocationTree = function(complete){
     });
     ltree.branch.append(rootnode);
     // Handle the rest of the tree
-    for(i = 1; i < rows.length; i++){
-      ltree.insert(new treelib.Node(null, {
-        'id' : rows[i].id,
-        'name' : rows[i].name
-      }), rows[i].inside_id, rows[i].id);
-    }
+    do{
+      var jump = false;
+      for(i = 1; i < rows.length; i++){
+        if(ltree.find(rows[i].inside_id) == null){
+          jump = true;
+        }
+        else if(ltree.find(rows[i].id) == null){
+          ltree.insert(new treelib.Node(null, {
+            'id' : rows[i].id,
+            'name' : rows[i].name
+          }), rows[i].inside_id, rows[i].id);
+        }
+      }
+    }while(jump);
     stmt.finalize();
     if(complete != undefined){
       complete();
@@ -96,4 +104,143 @@ this.printTreeRecurse = function(prefix, branch, callback){
 // List the locations
 this.getLocations = function(callback){
   this.printTreeRecurse('', this.locationTree.branch, callback);
+}
+
+// Add a category
+this.addCategory = function(name, successCB, failCB){
+  var stmt = this.dbm.makeStatement("INSERT INTO category (name) VALUES \
+  ($name)");
+  stmt.run({$name: name}, function(err, row){
+    if(err == null){
+      if(successCB != undefined){
+        successCB();
+      }
+    }
+    else{
+      if(failCB != undefined){
+        failCB();
+      }
+      throw err;
+    }
+  });
+  stmt.finalize();
+}
+
+// Delete a category
+this.deleteCategory = function(id, successCB, failCB){
+  var stmt = this.dbm.makeStatement("DELETE FROM category WHERE id = $id");
+  stmt.run({$id: id}, function(err, row){
+    if(err == null){
+      if(successCB != undefined){
+        successCB();
+      }
+    }
+    else{
+      if(failCB != undefined){
+        failCB();
+      }
+      throw err;
+    }
+  });
+  stmt.finalize();
+}
+
+// Count category members
+this.countCategoryMembers = function(id, callback){
+  var stmt = this.dbm.makeStatement("SELECT COUNT(*) AS num FROM item WHERE \
+  category_id = $id;");
+  stmt.get({$id: id}, function(err, row){
+    if(err == null){
+      callback(row.num);
+    }
+    else{
+      throw err;
+    }
+  });
+  stmt.finalize();
+}
+
+// Add location
+this.addLocation = function(name, insideID, successCB, failCB){
+  var stmt = this.dbm.makeStatement("INSERT INTO location (name, inside_id) \
+  VALUES ($name, $inID)");
+  stmt.run({$name: name, $inID: insideID}, function(err, row){
+    if(err == null){
+      if(successCB != undefined){
+        successCB();
+      }
+    }
+    else{
+      if(failCB != undefined){
+        failCB();
+      }
+      throw err;
+    }
+  });
+  stmt.finalize();
+}
+
+// Remove location
+this.deleteLocation = function(id, successCB, failCB){
+  var stmt = this.dbm.makeStatement("DELETE FROM location WHERE id = $id");
+  stmt.run({$id: id}, function(err, row){
+    if(err == null){
+      if(successCB != undefined){
+        successCB();
+      }
+    }
+    else{
+      if(failCB != undefined){
+        failCB();
+      }
+      throw err;
+    }
+  });
+  stmt.finalize();
+}
+
+// Count location members and sub-locations
+this.countLocationMembers = function(id, callback){
+  var stmt = this.dbm.makeStatement("SELECT COUNT(*) AS num FROM entity WHERE \
+  location_id = $id AND quantity > 0;");
+  var substmt = this.dbm.makeStatement("SELECT COUNT(*) AS sub FROM location \
+  WHERE inside_id = $id;")
+  stmt.get({$id: id}, function(err, row){
+    if(err == null){
+      var num = row.num;
+      substmt.get({$id: id}, function(err, row){
+        if(err == null){
+          callback([num, row.sub]);
+        }
+        else{
+          throw err;
+        }
+      });
+      substmt.finalize();
+    }
+    else{
+      throw err;
+    }
+  });
+  stmt.finalize();
+}
+
+// Update super-location of a location
+this.updateSuperLocation = function(id, superID, successCB, failCB){
+  var stmt = this.dbm.makeStatement("UPDATE location SET inside_id = $superid\
+  WHERE id = $id");
+  stmt.run({$id: id, $superid: superID}, function(err, row){
+    if(err == null){
+      if(successCB != undefined){
+        successCB();
+      }
+    }
+    else{
+      if(failCB != undefined){
+        failCB();
+      }
+      throw err;
+    }
+  });
+  stmt.finalize();
 }
--- app/page/inv/index.js
+++ app/page/inv/index.js
@@ -16,8 +16,8 @@ function makeModel(dbm){
 function insertRowIntoTable(table){
   var insertFunction = function(row){
     table.append("<tr><td><input type=\"checkbox\" id=\"entity-checkbox-" +
-                 row.id + "\" value=\"" + row.id + "\"></td><td>" + row.quantity +
-                 "</td><td>" + row.name + "</td><td>" + row.category +
+                 row.id + "\" value=\"" + row.id + "\"></td><td>" + row.quantity
+                 + "</td><td>" + row.name + "</td><td>" + row.category +
                  "</td><td>" + row.location + "</td></tr>");
   }
   return insertFunction;
@@ -26,8 +26,7 @@ function insertRowIntoTable(table){
 // Insert option into select-list function
 function insertOptionIntoSelect(select){
   var insertFunction = function(row){
-    select.append("<option data-id=\"" + row.id + "\">" + row.name +
-                  "</option>");
+    select.append("<option value=\"" + row.id + "\">" + row.name + "</option>");
   }
   return insertFunction;
 }
@@ -39,16 +38,35 @@ function updateLists(){
                      '#manage-delete-category'];
   for(i = 0; i < catListList.length; i++){
     var select = $(catListList[i]);
+    select.children().not('option[value="-1"]').remove();
     dbModel.getCategories(insertOptionIntoSelect(select));
   }
   // Push location lists
   var locListList = ['#filter-location', '#add-stuff-location',
                      '#manage-move-location-into', '#manage-move-location-from',
-                     '#manage-add-location', '#manage-delete-location'];
+                     '#manage-add-location-inside', '#manage-delete-location'];
   for(i = 0; i < locListList.length; i++){
     var select = $(locListList[i]);
+    select.children().not('option[value="-1"]').remove();
     dbModel.getLocations(insertOptionIntoSelect(select));
   }
+}
+
+// Post a notification to the given pane
+function notify(tabID, message, type){
+  var tab = $('#' + tabID);
+  var headrow = tab.prepend('<div class="row" style="display:\
+  none"></div>').children(':first');
+  var notif = headrow.append('<div\
+  class="col-md-12"></div>').children(':first');
+  notif = notif.append('<div class="alert"\
+  role="alert"></div>').children(':first');
+  notif.addClass('alert-' + type);
+  notif.text(message);
+  headrow.slideDown('fast');
+  notif.click(function(){
+    headrow.slideUp('fast');
+  });
 }
 
 // Site IO
@@ -62,4 +80,110 @@ $(document).ready(function(){
   // While we're at it, populate the interface selector lists
   dbModel.update(updateLists);
 
+  // Activate buttons
+  // Manage add-category
+  $('#manage-add-category-button').removeClass('disabled').click(function(){
+    var field = $('#manage-add-category');
+    var newCat = field.val();
+    if(newCat != ""){
+      dbModel.addCategory(newCat, function(){
+        field.val("");
+        dbModel.update(updateLists);
+        notify('manage-tab', "Successfully added category "+newCat, 'success');
+      }, function(){
+        notify('manage-tab', "Failed to add category "+newCat, 'danger');
+      });
+    }
+  });
+  // Manage remove-category
+  $('#manage-delete-category-button').removeClass('disabled').click(function(){
+    var target = $('#manage-delete-category').val();
+    // Early escape clauses. Help with indentation problems
+    if(target < 0){return;}
+    if(target == 1){
+      notify('manage-tab', "The Uncategorized category cannot be deleted",
+             'warning');
+      return;
+    }
+    var name = $('#manage-delete-category option[value=' + target + ']').text();
+    dbModel.countCategoryMembers(target, function(number){
+      if(number != 0){
+        notify('manage-tab', "Cannot delete nonempty category "+name,
+        'warning');
+        return;
+      }
+      dbModel.deleteCategory(target, function(){
+        dbModel.update(updateLists);
+        notify('manage-tab', "Successfully deleted category "+name, 'success');
+      }, function(){
+        notify('manage-tab', "Failed to delete category "+name, 'danger');
+      });
+    });
+  });
+  // Manage add-location
+  $('#manage-add-location-button').removeClass('disabled').click(function(){
+    var nameField = $('#manage-add-location-name');
+    var name = nameField.val();
+    var inside = $('#manage-add-location-inside').val();
+    if(name == "" || inside < 0){return;}
+    dbModel.addLocation(name, inside, function(){
+      nameField.val("");
+      dbModel.update(updateLists);
+      notify('manage-tab', "Successfully added location "+name, 'success');
+    }, function(){
+      notify('manage-tab', "Failed to add location "+name, 'danger');
+    });
+  });
+  // Manage delete-location
+  $('#manage-delete-location-button').removeClass('disabled').click(function(){
+    var target = $('#manage-delete-location').val();
+    var name = $('#manage-delete-location option[value=' + target +']').text();
+    if(target < 0){return;}
+    if(target == 1){
+      notify('manage-tab', "You are not allowed to delete the World",
+             'warning');
+      return;
+    }
+    dbModel.countLocationMembers(target, function(numbers){
+      var items = numbers[0];
+      var subloc = numbers[1];
+      if(items != 0){
+        notify('manage-tab', "Cannot delete nonempty location "+name,
+        'warning');
+        return;
+      }
+      if(subloc != 0){
+        notify('manage-tab', "Location "+name+" still has children; cannot"+
+               " delete", 'warning');
+        return;
+      }
+      dbModel.deleteLocation(target, function(){
+        dbModel.update(updateLists);
+        notify('manage-tab', "Successfully deleted location "+name, 'success');
+      }, function(){
+        notify('manage-tab', "Failed to delete location "+name, 'danger');
+      });
+    });
+  });
+  // Manage move-location
+  $('#manage-move-location-button').removeClass('disabled').click(function(){
+    var from = $('#manage-move-location-from').val();
+    var into = $('#manage-move-location-into').val();
+    if(from == null || into == null){return;}
+    if(from == into){
+      notify('manage-tab', "Cannot move a location into itself", 'warning');
+      return;
+    }
+    if(from == 1){
+      notify('manage-tab', "You cannot move the World", 'warning');
+      return;
+    }
+    var name = $('#manage-move-location-from option[value=' + from +']').text();
+    dbModel.updateSuperLocation(from, into, function(){
+      dbModel.update(updateLists);
+      notify('manage-tab', "Successfully moved location "+name, 'success');
+    }, function(){
+      notify('manage-tab', "Failed to move location "+name, 'danger');
+    });
+  });
 });
